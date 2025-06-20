e74440f41e51cd6ef77836d1900f9a8f4da0568d
AishwaryaS9 <aishwaryasudupi9@gmail.com>
2025-06-20 18:17:19 +0530

    feat: implemented landing page

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import BlogCategories from "../components/BlogCategories"
+import Hero from "../components/Hero"
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <BlogCategories />
+        </>
+    )
+}
+
+export default Home
